4b0411b8d73c7bc0b6f18cfdf66102fc6a526c25
Geraldo <profgeraldofip@gmail.com>
2021-09-09 13:13:36 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+
 
 function App() {
   return (
     <>
-      <Navbar/>
+    <Navbar/>
     
-        <div className="container">
+    <div className="container">
+    <h1 className="text-primary py-3">Painel de vendas</h1>
+
+    <div className="row px-3">
+
+      <BarChart/>
+
+
+    </div>
+
+
     <DataTable/>
 
-       </div>  
-     
-      <Footer/>
+    </div>  
+
+    <Footer/>
     </>
-  );
+    );
 }
 
 export default App;
